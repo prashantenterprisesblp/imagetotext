3c1d2293f3643194e8424f7791ce8610852a37a3
prashantenterprisesblp <prashantenterprisesblp@gmail.com>
2017-02-22 23:11:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -62,11 +62,11 @@ vision.detect(req.file.path, types, function(err, detections, apiResponse) {
       var texts = JSON.stringify(apiResponse.responses[0].textAnnotations[0].description);
       var textsHtmlwithoutQuotes = texts.replace(/"/g, '');
       var textWithNextline = textsHtmlwithoutQuotes.replace(/\n/g, '\\n');
-      console.log("Check texts ::>>" + textsHtml);
+      console.log("Check texts ::>>" + textWithNextline);
       // Write out the JSON output of the Vision API
        //res.write(JSON.stringify(jsonObj.textAnnotations, null, 4));
       
-      res.write('<p>' + textsHtml + '</p>', null, 4);
+      res.write('<p>' + textWithNextline + '</p>', null, 4);
 
       // Delete file (optional)
       fs.unlinkSync(req.file.path);
