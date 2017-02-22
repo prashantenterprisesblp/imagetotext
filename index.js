0b4775a07852c5713945093a8712ef1a97738a51
prashantenterprisesblp <prashantenterprisesblp@gmail.com>
2017-02-22 21:29:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ vision.detect(req.file.path, types, function(err, detections, apiResponse) {
       res.write('<img width=200 src="' + base64Image(req.file.path) + '"><br>');
 
       //var jsonOutput = JSON.parse(apiResponse);
-      var texts = apiResponse.textAnnotations.description;
+      var texts = JSON.stringify(apiResponse);
       console.log("Check texts" + texts);
       // Write out the JSON output of the Vision API
       res.write(JSON.stringify(apiResponse, null, 4));
